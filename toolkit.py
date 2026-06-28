88ada199217f73920e658d6da8c4e0b533e7a7ab
chaim <chchdeu@gmail.com>
2026-06-28 14:29:24 +0300

    mission_6

--- toolkit.py
+++ toolkit.py
@@ -20,3 +20,8 @@ greet_with_titel(name="chaim",titel="commander")
 def agent_detalis(active,name,level):
     print("name: ", name , "level: " ,level , "active: ", active)
 agent_detalis(level=5,name="agent x",active=True )    
+#mission 6
+def multiply(a,b=2):
+    print(a*b)
+multiply(5)
+multiply(5,5)
